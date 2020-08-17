b64c55f6dd724b8b80ea8ee896fa659e80474ab4
leonel47 <waboleonel@gmail.com>
2020-08-16 21:43:28 -0500

    Enabling data binding feature

--- app/src/main/java/com/aiwadev/notifintro/MainActivity.kt
+++ app/src/main/java/com/aiwadev/notifintro/MainActivity.kt
@@ -2,10 +2,16 @@ package com.aiwadev.notifintro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aiwadev.notifintro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bind: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Using data binding to inflate our layout
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
     }
 }
